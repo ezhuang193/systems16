18b44e9f1550de5d70875f51acfa1777a791e956
Eric Zhuang <Eric Zhuang>
2016-10-14 14:03:36 -0400

    memgrind

--- main.c
+++ main.c
@@ -11,3 +11,52 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
+// int main( int argc, char** argv) {
+
+// 	printf("1\n");
+// 	block_init();
+// 	printf("2\n");
+// 	int x;
+// 	printf("3\n");
+// 	free( &x );
+// 	printf("4\n");
+	
+// 	char * p = (char *)malloc( 200 );
+// 	printf("5\n");
+// 	free( p + 10 );
+// 	printf("6\n");
+	
+// 	int * y;
+// 	printf("7\n");
+// 	free( y );
+// 	printf("8\n");
+// 	char * p2 = (char*)malloc(100);
+// 	printf("9\n");
+// 	free( p2 );
+// 	printf("10\n");
+// 	free( p2 );
+// 	printf("11\n");
+// 	char* p4 = (char*)malloc(5001);
+// 	printf("12\n");
+// 	char* p5 = (char*)malloc(5000);
+// 	printf("13\n");
+// 	char* p6 = (char*)malloc(1);
+// 	printf("14\n");
+
+//    int i, n;
+//    time_t t;
+   
+//    n = 5;
+   
+//    /* Intializes random number generator */
+//    srand((unsigned) time(&t));
+
+//    /* Print 5 random numbers from 0 to 49 */
+//    for( i = 0 ; i < n ; i++ ) 
+//    {
+//       printf("%d\n", rand() % 2);
+//    }
+   
+// 	return 0;
+
+// }
---
+++ memgrind.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include "mymalloc.h"
+
+
+void testA(){
+	char* malloc_pointers[3000];
+	int pointer_counter = 0;
+	int workload_counter = 0;
+	long int time_total = 0;
+
+	while(workload_counter < 100) {
+		struct timeval start, end;
+		gettimeofday(&start, NULL);
+
+
+  		while(pointer_counter < 3000) {
+
+	  		malloc_pointers[pointer_counter] = malloc(1);
+	  		pointer_counter++;
+
+	  	}
+	  	pointer_counter = 0;
+	  	while(pointer_counter < 3000) {
+
+	  		free(malloc_pointers[pointer_counter]);
+	  		pointer_counter++;
+
+	  	}
+	  	gettimeofday(&end, NULL);
+	  	time_total = time_total + ((end.tv_sec * 1000000 + end.tv_usec)
+		- (start.tv_sec * 1000000 + start.tv_usec));
+		workload_counter++;
+	}
+	long int average_time = time_total/100;
+	printf("Average Time for Test A was %ld microseconds\n", average_time);	
+}
+
+void testB(){
+	int pointer_counter = 0;
+	int workload_counter = 0;
+	long int time_total = 0;
+
+	while(workload_counter < 100) {
+		struct timeval start, end;
+		gettimeofday(&start, NULL);
+
+
+  		while(pointer_counter < 3000) {
+
+	  		char* test_pointer = (char*)malloc(1);
+	  		free(test_pointer);
+	  		pointer_counter++;
+	  	}
+
+	  	gettimeofday(&end, NULL);
+	  	time_total = time_total + ((end.tv_sec * 1000000 + end.tv_usec)
+		- (start.tv_sec * 1000000 + start.tv_usec));
+		workload_counter++;
+	}
+	long int average_time = time_total/100;
+	printf("Average Time for Test B was %ld microseconds\n", average_time);	
+
+}
+
+void testC(){
+
+   int malloc_counter = 0;
+   int free_counter = 0;
+   int total_counter = 0;
+   time_t t;
+  
+   /* Intializes random number generator */
+   srand((unsigned) time(&t));
+
+   while(total_counter < 6000) {
+
+   		int random_number = rand() % 2;
+   		if(random_number == 0 && malloc_counter < 3000) {
+   			malloc_counter++;
+   			total_counter++;
+   		}else if (random_number == 1 && free_counter < 3000) {
+   			free_counter++;
+   			total_counter++;
+   		}
+
+   }
+   printf("total_counter is: %d\nmalloc_counter is: %d\nfree_counter is: %d\n",total_counter,malloc_counter,free_counter);
+
+}
+void testD(){
+
+}
+
+void testEF(){
+
+}
+
+
+int main( int argc, char** argv ) {
+	block_init();
+	testA();
+	testB();
+	testC();
+    
+    return 0;
+}
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Status Help
